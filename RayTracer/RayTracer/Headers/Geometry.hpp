1e82a152a88f5407484466f395a35a5209c44935
Ylva Selling <ylva.selling@gmail.com>
2019-12-10 11:44:50 +0100

    Implemented dynamic binding for geometry
    Triangles and spheres are now in the same vector.

---
+++ RayTracer/RayTracer/Headers/Geometry.hpp
@@ -0,0 +1,14 @@
+//
+//  Geometry.hpp
+//  AGIR
+//
+//  Created by Ylva Selling on 2019-12-10.
+//  Copyright © 2019 Ylva Selling. All rights reserved.
+//
+
+#ifndef Geometry_hpp
+#define Geometry_hpp
+
+#include <stdio.h>
+
+#endif /* Geometry_hpp */
---
+++ RayTracer/RayTracer/Source/Geometry.cpp
@@ -0,0 +1,9 @@
+//
+//  Geometry.cpp
+//  AGIR
+//
+//  Created by Ylva Selling on 2019-12-10.
+//  Copyright © 2019 Ylva Selling. All rights reserved.
+//
+
+#include "Geometry.hpp"
